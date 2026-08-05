5064c1347e0474ab37776b2fb5957b8d00f35027
Hemnath K K <hemnathkandasamyk@gmail.com>
2026-08-05 15:53:27 +0530

    Create 1667. Fix Names in a Table.sql

---
+++ LeetCode_Problems/1667. Fix Names in a Table.sql
@@ -0,0 +1,12 @@
+#1667. Fix Names in a Table
+
+SELECT
+   user_id,
+   CONCAT(
+     UPPER(LEFT(name,1)),
+     LOWER((SUBSTRING(name,2,LENGTH(name))))
+    ) AS name
+FROM
+Users
+ORDER BY
+user_id;
