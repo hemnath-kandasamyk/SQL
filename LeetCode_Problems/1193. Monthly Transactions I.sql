eaf4bf609161f4078bc6d0c6d6c572b058567a19
Hemnath K K <hemnathkandasamyk@gmail.com>
2026-08-12 14:24:16 +0530

    Create 1193. Monthly Transactions I.sql

---
+++ LeetCode_Problems/1193. Monthly Transactions I.sql
@@ -0,0 +1,11 @@
+#1193. Monthly Transactions I
+SELECT 
+DATE_FORMAT(trans_date,'%Y-%m') AS month,
+country,
+count(*) as trans_count,
+sum(CASE WHEN STATE = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+SUM(amount) AS trans_total_amount,
+SUM(CASE WHEN STATE = 'approved' THEN amount ELSE 0 END ) AS approved_total_amount
+ FROM
+Transactions A
+GROUP BY month,country;
