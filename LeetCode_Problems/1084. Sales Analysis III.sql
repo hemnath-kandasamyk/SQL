e02c45689389d455bacee585840febf542e2c04f
Hemnath K K <hemnathkandasamyk@gmail.com>
2026-09-14 10:39:09 +0530

    Add SQL query for Sales Analysis III

---
+++ LeetCode_Problems/1084. Sales Analysis III.sql
@@ -0,0 +1,17 @@
+# 1084. Sales Analysis III
+SELECT
+    product_id,
+    product_name
+FROM
+    Product
+WHERE
+    product_id in
+    (
+        SELECT
+            product_id
+        FROM
+            Sales
+        GROUP BY
+            product_id
+        HAVING MIN(sale_date)>='2019-01-01' AND MAX(sale_date)<='2019-03-31'    
+    )        
