7499d47f1ef39dbd2f47aa4c15dcebbf77a4504f
Hemnath K K <hemnathkandasamyk@gmail.com>
2026-09-15 08:59:26 +0530

    Create 1693. Daily Leads and Partners.sql

---
+++ LeetCode_Problems/1693. Daily Leads and Partners.sql
@@ -0,0 +1,10 @@
+# 1693. Daily Leads and Partners 
+SELECT
+    date_id,
+    make_name,
+    COUNT(DISTINCT lead_id) unique_leads,
+    COUNT(DISTINCT partner_id) unique_partners
+FROM 
+    DailySales    
+GROUP BY 
+    date_id,make_name
