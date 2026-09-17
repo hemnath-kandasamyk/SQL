442ac92946c04f177db4017bc5b296684194f33d
Hemnath K K <hemnathkandasamyk@gmail.com>
2026-09-17 10:31:24 +0530

    Add SQL query for Market Analysis I

---
+++ LeetCode_Problems/1158. Market Analysis I.sql
@@ -0,0 +1,12 @@
+# 1158. Market Analysis I
+SELECT
+    user_id AS buyer_id,
+    join_date,
+    SUM(IF(YEAR(order_date) ='2019',1,0)) AS orders_in_2019
+FROM
+    Users u
+LEFT JOIN
+    Orders o
+ON
+    u.user_id = o.buyer_id
+GROUP BY user_id
